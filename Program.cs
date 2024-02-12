25040909a97eca3e2d98d396f7ca6842f469b3b6
= <=>
2024-02-12 11:39:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// exercício 01 - criar uma variável chamada curso
+string curso = "C#: criando sua primeira aplicação";
+
+// exercício 02 - mostrando o conteúdo no console
+Console.WriteLine(curso);
+
+// exercício 03 - criando uma variável nome e sobrenome
+string nome = "Juliana";
+string sobrenome = "Moreira Kinoshita";
+Console.WriteLine(nome);
+Console.WriteLine(sobrenome);
+
+// exercício 04 - mostrar o nome dos instrutores
+Console.WriteLine("Daniel Portugal e Gui Lima");
+
+//exercício 05 - Nota média para aprovação
+int notaMedia = 8;
+if (notaMedia >= 5)
+{
+    Console.WriteLine("Nota suficiente para aprovação.");
+}
+else
+{
+    Console.WriteLine("Nota insuficiente para a aprovação.");
+}
+
+//exercício 06 - Criar uma lista de linguagens
+
+void exibirListaDeLinguagens()
+{
+    Console.WriteLine("C#");
+    Console.WriteLine("Java");
+    Console.WriteLine("JavaScript");
+}
+
+exibirListaDeLinguagens();
+
+//exercício 07 - Exibir o valor de C# da lista anterior
+void exibirLista()
+{
+    Console.WriteLine("C#");
+}
+
+exibirLista();
+
+//exercício 08 - Exibir a n-ésima posição em uma lista
+//int posicao = int(Console.read());
+//Console.WriteLine(lista[posicao]); - exercicio com erro!
